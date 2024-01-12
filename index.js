85ddc2cd56d488f12a096f2bd50891290a79372d
Ravano Akbar Widodo <118270814+ravano-2464@users.noreply.github.com>
2024-01-12 14:58:42 +0700

    Bootcamp Personal Web Day 12

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-const port = 5000
+const port = 7000
 
-// app.set = buat setting varible global, configuratoin, dll
 app.set("view engine", "hbs")
 app.set("views", path.join(__dirname, 'src/views'))
 
 app.use("/assets", express.static(path.join(__dirname, 'src/assets')))
-app.use(express.urlencoded({ extended: false })) // body parser, extended : false -> querystring, extended : true -> menggunakan querystring third party -> qs
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/', home)
 app.get('/contact', contact)
-app.get('/My-Project', blog)
-app.get('/add-My-Project', addBlogView)
-app.post('/add-My-Project', addBlog)
+app.get('/My-Project', MyProject)
+app.get('/add-My-Project', addMyProjectView)
+app.post('/add-My-Project', addMyProject)
 
-app.get('/My-Project-detail/:id', blogDetail)
+app.get('/My-Project-detail/:id', MyProjectDetail)
 app.get('/testimonial', testimonial)
 
 function home(req, res) {
@@ -27,7 +26,7 @@ function contact(req, res) {
     res.render('contact')
 }
 
-function blog(req, res) {
+function MyProject(req, res) {
     const data = [
         {
             title: "Title 1",
@@ -46,20 +45,20 @@ function blog(req, res) {
     res.render('My-Project', { data })
 }
 
-function addBlogView(req, res) {
+function addMyProjectView(req, res) {
     res.render('add-My-Project')
 }
 
-function addBlog(req, res) {
+function addMyProject(req, res) {
     const { title, content } = req.body
 
-    alert("Title :", title)
-    alert("Content :", content)
-    res.redirect('My-Project')
+    console.log("Title :", title)
+    console.log("Content :", content)
+    res.redirect('/My-Project') 
 }
 
-function blogDetail(req, res) {
-    const { id } = req.params // destructuring
+function MyProjectDetail(req, res) {
+    const { id } = req.params
 
     const title = "Title 1"
     const content = "Content 1"
