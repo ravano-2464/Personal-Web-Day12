2eb06d37650801619aa8dc935916bcd333b92685
Ravano Akbar Widodo <118270814+ravano-2464@users.noreply.github.com>
2024-01-17 21:54:24 +0700

    Bootcamp Personal Web Day 12

--- index.js
+++ index.js
@@ -18,6 +18,12 @@ app.post('/add-My-Project', addMyProject)
 app.get('/My-Project-detail/:id', MyProjectDetail)
 app.get('/testimonial', testimonial)
 
+app.get('/update-My-project/:id', updateMyProject)
+app.post('/update-My-Project', updateMyProject)
+
+app.get('/My-Project-detail/:id', MyProjectDetail)
+app.get('/testimonial', testimonial)
+
 function home(req, res) {
     res.render('index')
 }
@@ -59,6 +65,44 @@ function addMyProject(req, res) {
 
 function MyProjectDetail(req, res) {
     const { id } = req.params
+
+    const title = "Title 1"
+    const content = "Content 1"
+
+    const data = {
+        id,
+        title,
+        content
+    }
+
+    res.render('My-Project-detail', { data })
+}
+
+
+function updateMyProject(req, res) {
+    const { title, content, id } = req.body
+
+    console.log("Id :", id)
+    console.log("Title :", title)
+    console.log("Content :", content)
+
+    data[parseInt(id)] = {
+        title, 
+        content,
+    }
+    
+    res.redirect('/My-Project')
+}
+
+function deleteMyProject(req, res) {
+    const { id } = req.params
+
+    data.splice(id, 1)
+    res.redirect('/My-Project')
+}
+
+function MyProjectDetail(req, res) {
+    const { id } = req.params 
 
     const title = "Title 1"
     const content = "Content 1"
